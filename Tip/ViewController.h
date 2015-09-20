ee48495419cf05b611e2a0190afadd3bc041eb78
Abe Schonfeld <aschonfeld@groupon.com>
2015-09-20 14:45:36 -0400

    Initial Commit

---
+++ Tip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip
+//
+//  Created by Abe Schonfeld on 9/20/15.
+//  Copyright (c) 2015 Abe Schonfeld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
