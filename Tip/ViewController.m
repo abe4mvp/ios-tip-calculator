1b87a58bc40f00fda88a4ca1355e2bdf00dfc7a6
Abe Schonfeld <aschonfeld@groupon.com>
2015-09-22 22:45:48 -0400

    ability to save default tip amount  and keyboard always on

--- Tip/ViewController.m
+++ Tip/ViewController.m
@@ -25,13 +25,16 @@
     [self updateValues];
 }
 
-- (IBAction)tipChanged:(id)sender {
+- (IBAction)tipChanged {
     [self updateValues];
     
 }
 
+- (IBAction)saveDefaultPressed:(id)sender {
+    [self setDefaultTip];
+}
 
--(void)updateValues {
+- (void)updateValues {
     float tipPercent = [self getTipPercent];
     float billAmount = [self.billTextField.text floatValue];
     
@@ -40,8 +43,30 @@
     self.totalLabel.text = [NSString stringWithFormat:@"%.02f", (1 + tipPercent) * billAmount];
 }
 
--(float)getTipPercent {
+- (float)getTipPercent {
     return roundf(self.tipSlider.value) / 100;
+}
+
+- (void)setDefaultTip {
+    float currentTip = self.tipSlider.value;
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setFloat:(currentTip) forKey:@"defaultTip"];
+}
+
+- (float)getDefaultTip {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+
+    if ([defaults objectForKey:@"defaultTip"] != nil) {
+        return [defaults floatForKey:@"defaultTip"];
+    } else {
+        return 15.0;
+    }
+}
+
+
+
+- (void)setTipToDefault {
+    self.tipSlider.value = [self getDefaultTip];
 }
 
 
@@ -50,6 +75,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     [self.tipSlider setMaxFractionDigitsDisplayed:0];
+    self.tipSlider.value = [self getDefaultTip];
+}
+
+- (void) viewDidAppear:(BOOL)animated {
+    [self.billTextField becomeFirstResponder];
 }
 
 - (void)didReceiveMemoryWarning {
